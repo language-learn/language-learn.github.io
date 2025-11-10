fa55d788d6fdd112dca0675f469bea2743f52fed
language-learn <language-learn@users.noreply.github.com>
2025-11-10 13:10:57 +0100

    updated .gitignore

--- serve.sh
+++ serve.sh
@@ -4,9 +4,10 @@
 # Usage: ./serve.sh
 
 podman run --rm \
+  --userns=keep-id \
   --volume="$PWD:/srv/jekyll:Z" \
   --publish 4000:4000 \
   --publish 35729:35729 \
   -it \
   docker.io/jekyll/jekyll:4 \
-  jekyll serve --watch --force_polling --livereload
+  jekyll serve --watch --force_polling --livereload --host 0.0.0.0
